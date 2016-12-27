5833e5732f5630bf49060ff9f1d881efd37f8c24
Zac Heacker <zac.heacker@gmail.com>
2016-12-27 14:47:32 -0600

    at least it's working

---
+++ 0. Docker/rscripts/installpkgs.R
@@ -0,0 +1,18 @@
+## installs the following packages on top of the rocker/hadleyverse docker image
+## this list should include all packages required for the johns hopkins data science specialization
+
+toInstall <- c(
+     "car",
+     "ezknitr",
+     "formatR",
+     "GGally",
+     "kernlab",
+     "UsingR",
+     "wesanderson"
+)
+
+install.packages(toInstall, quiet=TRUE)
+
+rm(toInstall)
+
+# setwd("/opt/project")
